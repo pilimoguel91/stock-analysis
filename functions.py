1c45183dbaa1a9ef32b7776eabe7f55816d2c18f
Pili Moguel <43249271+pilimoguel91@users.noreply.github.com>
2025-01-09 15:36:44 +0100

    add_function_cashflow

--- functions.py
+++ functions.py
@@ -5,20 +5,22 @@ import json
 with open('IBM_CASH_FLOW.json') as json_cashflow_data: ### Missing: convert to variables for different stock symbols
     data_cashflow = json.load(json_cashflow_data)
     df_cashflow = pd.DataFrame(data_cashflow['annualReports'])
-    df_cashflow['year'] = df_cashflow['fiscalDateEnding'][:4]
-    df_cashflow
+    fiscalDateEnding = pd.to_datetime(df_cashflow['fiscalDateEnding'])
+    df_cashflow['year'] = pd.DatetimeIndex(fiscalDateEnding).year
+    df_cashflow['operatingCashflow'] = pd.to_numeric(df_cashflow['operatingCashflow'], errors="coerce")
+    df_cashflow['capitalExpenditures'] = pd.to_numeric(df_cashflow['capitalExpenditures'], errors="coerce")
 
 with open('IBM_OVERVIEW.json') as json_overview_data: ### Missing: convert to variables for different stock symbols
     data_overview = json.load(json_overview_data)
-    df_overview = pd.DataFrame(data_overview, index=[0])
-    df_overview
+    df_overview = pd.DataFrame(data_overview, index=[0])    
+    df_overview['MarketCapitalization'] = pd.to_numeric(df_overview['MarketCapitalization'], errors="coerce")
+    LatestQuarter = pd.to_datetime(df_overview['LatestQuarter'])
+    df_overview['previous_year'] = (pd.DatetimeIndex(LatestQuarter).year - 1)
 
 
-print(df_cashflow['year'])
-# def cash_4_owners():
-#    df_cashflow['cash_4_owners'] = df_cashflow['operatingCashflow'] - df_cashflow['capitalExpenditures']
-#    df_overview['ten_cap'] = df_overview['MarketCapitalization'] /  df_cashflow['cash_4_owners']
-#    df_overview['ten_cap']
-
-# cash_4_owners()
+def cash_4_owners(year = int(df_overview['previous_year'])):
+    df_cashflow['cash_4_owners'] = df_cashflow['operatingCashflow'] - df_cashflow['capitalExpenditures']
+    df_cashflow_filter = df_cashflow.loc[df_cashflow['year'] == year]
+    df_overview['ten_cap'] = (df_overview['MarketCapitalization'] /  df_cashflow_filter['cash_4_owners'])
+    print(df_overview, df_cashflow)
 
