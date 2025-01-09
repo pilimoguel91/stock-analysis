ee4679a57f08e76a88955ec3f02e57776fefb5e2
Pili Moguel <43249271+pilimoguel91@users.noreply.github.com>
2025-01-09 15:55:45 +0100

    correct_function_add_prints

--- functions.py
+++ functions.py
@@ -17,10 +17,18 @@ with open('IBM_OVERVIEW.json') as json_overview_data: ### Missing: convert to va
     LatestQuarter = pd.to_datetime(df_overview['LatestQuarter'])
     df_overview['previous_year'] = (pd.DatetimeIndex(LatestQuarter).year - 1)
 
-
-def cash_4_owners(year = int(df_overview['previous_year'])):
+# Cashflow function
+def cash_4_owners(year = int(df_overview['previous_year'].iloc[0])):
+# Cash for owners calculates the cash available after capital expenditures, and is used to calculate the 10cap by using the market cap of the company.
     df_cashflow['cash_4_owners'] = df_cashflow['operatingCashflow'] - df_cashflow['capitalExpenditures']
     df_cashflow_filter = df_cashflow.loc[df_cashflow['year'] == year]
-    df_overview['ten_cap'] = (df_overview['MarketCapitalization'] /  df_cashflow_filter['cash_4_owners'])
-    print(df_overview, df_cashflow)
+# 10 cap is a yield return percentage based on the cash available divided by the company total value, above 10 is a good investment
+    df_overview['ten_cap'] = (df_cashflow_filter['cash_4_owners'] / df_overview['MarketCapitalization'])
+    yield_return = df_overview['ten_cap'].iloc[0]
+# Dummy print for now, we should pass this result to the model later, of course
+    if (df_overview['ten_cap'] > 0.10).all():
+        print(df_overview["Symbol"] + " looks like a good investment with a " + str(df_overview['ten_cap']) + " estimated yield return") 
+    else:
+        print(df_overview["Symbol"] + " doesnt look like a good investment with a " + str(yield_return) + " estimated yield return") 
 
+cash_4_owners()
