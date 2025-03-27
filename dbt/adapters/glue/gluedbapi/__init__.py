a95774d239d2a2d844604b1018a7e3c5ba856f99
Pili Moguel <43249271+pilimoguel91@users.noreply.github.com>
2025-03-27 15:13:57 +0100

    dbt-all

--- dbt/adapters/glue/gluedbapi/__init__.py
+++
@@ -1,3 +0,0 @@
-from dbt.adapters.glue.gluedbapi.cursor import GlueCursor,GlueDictCursor
-from dbt.adapters.glue.gluedbapi.connection import GlueConnection
-
--- dbt/include/glue/tests/generic/builtin.sql
+++
@@ -1,7 +0,0 @@
-{% test relationships(model, column_name, to, field) %}
-    {{ glue__test_relationships(model, column_name, to, field) }}
-{% endtest %}
-
-{% test accepted_values(model, column_name, values, quote=True) %}
-    {{ glue__test_accepted_values(model, column_name, values, quote) }}
-{% endtest %}
